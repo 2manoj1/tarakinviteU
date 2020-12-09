d61bfbd32f9f3ccc71f595dbbcd038248eb48dbf
Manoj Mukherjee <manojmukherjee777@gmail.com>
2020-12-09 21:29:51 +0530

    first commit

---
+++ src/components/Contact.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import wp from '../images/w.png'
+
+const Contact = () => {
+    return ( 
+<footer>
+		<div className="container center-text">
+			<ul className="social-icons">
+				<li><a href="https://wa.me/919475348143" target="_blank" rel="noopener noreferrer"><img alt="WP" src={wp} /></a></li>
+			</ul>
+		</div>
+	</footer>
+        );
+    }
+    
+    export default Contact;
---
+++ src/components/Weadingloaction.js
@@ -0,0 +1,65 @@
+import React from 'react';
+
+const Weadingloaction = () => {
+    return ( <section className="section w-details-area center-text">
+		<div className="container">
+			<div className="row">
+				<div className="col-sm-1"></div>
+				<div className="col-sm-10">
+					
+					<div className="heading">
+						<h2 className="title">Wedding & Reception Details</h2>
+						<span className="heading-bottom"><i className="icon icon-star"></i></span>
+					</div>
+					
+					<div className="wedding-details margin-bottom">
+						
+						<div className="w-detail right">
+							<i className="icon icon-ciurclke"></i>
+							<h4 className="title">WEDDING CEREMONY</h4>
+                            <p>Monday, March 1st, 2021</p>
+                            <p><i className="icon icon-email-plane"></i></p>
+                            <p>Anturibana, Bankura, West Bengal</p>
+                            <p>
+							<div className="map-responsive">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1091.1795696378815!2d86.83518782922742!3d23.333848230469048!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDIwJzAxLjkiTiA4NsKwNTAnMDguNyJF!5e1!3m2!1sen!2sin!4v1607528962790!5m2!1sen!2sin" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen=""></iframe>
+                            </div>
+							</p>
+
+						</div> 
+						
+						<div className="w-detail right">
+							<i className="icon icon-wine-glass"></i>
+							<h4 className="title">DINNER</h4>
+							<p>Time: 7PM Onwards</p>
+						</div> 
+						
+						<div className="w-detail left">
+							<i className="icon icon-cake"></i>
+							<h4 className="title">RECEPTION PARTY</h4>
+							<p>Tuesday, March 2nd, 2021</p>
+                            <p><i className="icon icon-email-plane"></i></p>
+                            <p>Neckrakunda, Bankura, West Bengal</p>
+                            <p>
+							<div className="map-responsive">
+							<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d1091.505043958882!2d86.85097682922743!3d23.29419823083761!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zMjPCsDE3JzM5LjEiTiA4NsKwNTEnMDUuNSJF!5e1!3m2!1sen!2sin!4v1607528792920!5m2!1sen!2sin" width="600" height="450" frameBorder="0" style={{border:0}} allowFullScreen=""></iframe>
+                                </div>
+								</p>
+						</div> 
+						
+						<div className="w-detail left">
+							<i className="icon icon-wine-glass"></i>
+							<h4 className="title">DINNER</h4>
+							<p>Time: 7PM Onwards</p>
+						</div> 
+						
+					</div>
+					
+				</div>
+			</div>
+		</div>
+    </section>    
+    );
+}
+
+export default Weadingloaction;
